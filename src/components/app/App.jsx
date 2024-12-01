630c599b09d24cc78a0674bc4cb3772bcadec447
thoughtowner <ilya.danilov.dev@gmail.com>
2024-12-01 05:01:45 +0300

    feat: add routes in App.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import MainPage from '../../pages/main-page/MainPage';
+import ServicesPage from '../../pages/services-page/ServicesPage';
+import ServicePage from '../../pages/service-page/ServicePage';
+import LoginPage from '../../pages/login-page/LoginPage';
+import ProfilePage from '../../pages/profile-page/ProfilePage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element= { <MainPage/> } />
+        <Route path='/services' element = { <ServicesPage/> } />
+        <Route path='/services/:id' element = { <ServicePage/> } />
+        <Route path='/login' element = { <LoginPage/> } />
+        <Route path='/profile' element = { <ProfilePage/> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
